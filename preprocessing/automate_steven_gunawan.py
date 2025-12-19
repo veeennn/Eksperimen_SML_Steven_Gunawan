cf998da809a3bd003f7e355a1ff85ff6d75ccc57
veeennn <sssss222555888@gmail.com>
2025-12-19 15:02:19 +0700

    Update automate_steven_gunawan.py

--- preprocessing/automate_steven_gunawan.py
+++ preprocessing/automate_steven_gunawan.py
@@ -6,7 +6,7 @@ from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.model_selection import train_test_split
 import pandas as pd
 
-
+data = pd.read_csv("kc_house_data_raw.csv")
 # Menentukan fitur numerik dan kategoris
 numeric_features = data.select_dtypes(include=['float64', 'int64']).columns.tolist()
 categorical_features = data.select_dtypes(include=['object']).columns.tolist()
@@ -46,4 +46,5 @@ preprocessor = ColumnTransformer(
         ('cat', categorical_transformer, categorical_features)
     ]
 )
-    
+dfApa = preprocessor.fit_transform(data)
+dfApa.tocsv("ty.csv")
